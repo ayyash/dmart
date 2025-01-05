9426ea89e5f136d09278dc413eec180bb82f2d1f
ayyash <abdul_ayyash@yahoo.com>
2025-01-05 13:44:40 +0300

    deployed

--- client/chunk-4GN7QOP4.js
+++
@@ -1,1 +0,0 @@
-import{Ab as a}from"./chunk-QUNHBVRW.js";var r=class{static MapLanguage(t){return t?t[a.language]:""}};export{r as a};
---
+++ client/chunk-Z3BPB6WE.js
@@ -0,0 +1,1 @@
+import{zb as a}from"./chunk-ZJ25XDQ3.js";var r=class{static MapLanguage(t){return t?t[a.language]:""}};export{r as a};
