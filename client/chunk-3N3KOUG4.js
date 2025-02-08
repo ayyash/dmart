16436f42c6907e5c412a20702926feea200e1161
ayyash <abdul_ayyash@yahoo.com>
2025-02-08 19:37:19 +0300

    cookie

---
+++ client/chunk-3N3KOUG4.js
@@ -0,0 +1,1 @@
+import{Bb as a}from"./chunk-4JK7GZYK.js";var r=class{static MapLanguage(t){return t?t[a.language]:""}};export{r as a};
--- client/chunk-NFFTWQX6.js
+++
@@ -1,1 +0,0 @@
-import{Bb as a}from"./chunk-MOKJMFP6.js";var r=class{static MapLanguage(t){return t?t[a.language]:""}};export{r as a};
